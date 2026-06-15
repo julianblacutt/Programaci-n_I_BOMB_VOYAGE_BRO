e9cac8ae34e195ba8d93093997e67eaaa6f84b58
julianblacutt <julian.blacutt@ucb.edu.bo>
2026-06-15 16:59:32 -0400

    Presentación y menú de dificultades

--- BOMB VOYAGE BRO JBT JASV/Bomb_Voyage_Bro.cpp
+++ BOMB VOYAGE BRO JBT JASV/Bomb_Voyage_Bro.cpp
@@ -19,10 +19,67 @@ struct DatosBomba
     int interruptores; //todavía en "veremos"
 }; //Algunas mecánicas pueden llegar a ser expandidas o eliminadas según nuestra conveniencia
 
+void desplegarMenu();
 int main()
 {
     system ("chcp 65001");
     system ("cls");
     
+    cout<<"ADVERTENCIA: Previo a iniciar el juego usted necesita el manual de"<<endl;
+    cout<<"desarmado de bombas descargado en PDF o en físico. Mucha suerte!"<<endl;
+    system("pause");
+    system ("cls");
+    cout<<"Bienvenid@s a..."<<endl;
+    cout<<"==============="<<endl;
+    cout<<"BOMB VOYAGE BRO"<<endl;
+    cout<<"==============="<<endl;
+    cout<<"Un juego hecho para la materia Programación I tomando como"<<endl;
+    cout<<"inspiración a llamado KEEP TALKING and NOBODY EXPLODES."<<endl;
+    cout<<"Junto a un compañero deberán ser buenos comunicándose y actuando"<<endl;
+    cout<<"bajo presión, recuerda que... el tiempo corre >:)"<<endl;
+    system("pause");
+    system("cls");
+    desplegarMenu();
     return 0;
+}
+void desplegarMenu()
+{
+    int op=0;
+    do
+    {
+        cout<<"MENÚ DE DIFICULTADES"<<endl;
+        cout<<"--------------------"<<endl;
+        cout<<"¿Qué tan experimentados son desarmando bombas?"<<endl;
+        cout<<"1. CARTUCHERO (NOVATO)"<<endl;
+        cout<<"2. C4 TR3RO (MEDIO)"<<endl;
+        cout<<"3. AMO DEL TSAR (PROFESIONAL)"<<endl;
+        cout<<"4. Salir del Programa"<<endl;
+        cout<<"Ingrese una opción válida por favor: ";
+        cin>>op;
+        system("cls");
+        switch (op)
+        {
+        case 1:
+            cout<<"Usted a eligió la dificultad CARTUCHERO"<<endl;
+            system("pause");
+            break;
+        case 2:
+            cout<<"Usted eligió la dificultad C4 TR3RO"<<endl;
+            system("pause");
+            break;
+        case 3:
+            cout<<"Usted eligió la dificultad AMO DEL TSAR"<<endl;
+            system("pause");
+            break;
+        default:
+            if (op<=0||op>4)
+            {
+                cout<<"Debe ingresar una opción válida."<<endl;
+                system("pause");
+            }
+            break;
+        }
+    } while (op!=4);
+    system("cls");
+    cout<<"Usted eligió: Salir del Juego"<<endl;
 }
